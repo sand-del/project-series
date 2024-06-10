6873011a51f9a73d19c17fc8932a6adf747f5f25
Sandra DELIMOGE <sandradelimoge@gmail.com>
2024-06-10 16:56:52 +0200

    module 2 & module 3

---
+++ src/Controller/MainController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+///**
+// * autre possibilité pour les versions inférieurs à PHP8
+// * @Route('main', name='app_main')
+// */
+
+class MainController extends AbstractController
+{
+    //équivalent comme les annotations sur java spring boot @Controller
+    #[Route('/home', name: 'main_home')]//prefix le nom du controller, suffixe le nom de la méthode
+    #[Route('', name: 'main_home_2')]
+    public function home(): Response
+    {
+        return $this->render('main/home.html.twig');
+    }
+
+    #[Route('/test', name: 'main_test')]
+    public function test(): Response
+    {
+        return $this->render('main/test.html.twig');
+    }
+
+}
