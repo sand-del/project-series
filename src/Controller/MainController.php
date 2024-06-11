8306a78e62e7285c7dba3bd3a5f2361f431d120a
Sandra DELIMOGE <sandradelimoge@gmail.com>
2024-06-11 14:11:20 +0200

    fin module 3

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -24,7 +24,16 @@ class MainController extends AbstractController
     #[Route('/test', name: 'main_test')]
     public function test(): Response
     {
-        return $this->render('main/test.html.twig');
+        $serie = ['title' => 'GOT', 'year' => '2011'];
+        $serie2 = ['title' => 'The Boys', 'year' => '2019'];
+
+        $username = '<h1>Sandra</h1>';
+
+        return $this->render('main/test.html.twig', [
+            'got' => $serie,
+            'boys' => $serie2,
+            'username' => $username
+        ]);
     }
 
 }
